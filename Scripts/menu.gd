5a725de46d1ef9af934e340ac1308208542485d8
glorpffed <157213336+glorpffed@users.noreply.github.com>
2026-02-06 20:39:06 +0500

    d

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Start"):
-		get_tree().change_scene_to_file("res://node_2d.tscn")
+		get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
 	if Input.is_action_just_pressed("Exit"):
 		get_tree().quit()
 
@@ -11,4 +11,4 @@ func _on_exit_pressed() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
